efd8d8fa552496aa7de048de56a46af58350c93f
Иван Усенков <mutator08@mail.ru>
2017-10-21 01:57:28 +0300

    first

---
+++ AwesomeAsset.php
@@ -0,0 +1,8 @@
+<?php
+namespace mutator08\assets;
+
+class AwesomeAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@bower/font-awesome/css';
+	public $css = ["font-awesome.min.css"];
+}
