92a80073ff5ab1abe01392826460790acc11775c
Иван Усенков <mutator08@mail.ru>
2017-10-21 02:21:02 +0300

    fix

--- AwesomeAsset.php
+++ AwesomeAsset.php
@@ -3,6 +3,6 @@ namespace mutator08\assets;
 
 class AwesomeAsset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@bower/font-awesome/css';
-	public $css = ["font-awesome.min.css"];
+    public $sourcePath = '@bower/font-awesome';
+	public $css = ["css/font-awesome.min.css"];
 }
